6d3f987ce8461714d70913e3eec30ef7553087b6
Victor Fernando Ferreira Gomes <victorgbpk@gmail.com>
2022-05-07 23:41:28 -0300

    Add port env variable

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,10 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({
-	origin: 'http://localhost:3000'
-}));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
 	console.log("HTTP server running!");
 });
